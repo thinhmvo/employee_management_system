130d746292f148fa00859d16236060045f164ebf
Thinh Vo <thinhmvo101@gmail.com>
2023-10-24 15:05:12 -0400

    regenerate spring project due to incompatible java version

--- springboot_be/src/main/java/com/tmvo/springboot_be/controller/EmployeeController.java
+++ springboot_be/src/main/java/com/tmvo/springboot_be/controller/EmployeeController.java
@@ -1,4 +1,23 @@
 package com.tmvo.springboot_be.controller;
 
+import com.tmvo.springboot_be.model.Employee;
+import com.tmvo.springboot_be.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
 }
